e83213142094e997becfd2279ce6973636747227
Pradnya Pagale <pradnyapagale7654@gmail.com>
2026-04-11 03:11:18 +0530

    Task form added

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Your Study Planner Dashboard</p>
+    </div>
+  );
+}
+
+export default Dashboard;
